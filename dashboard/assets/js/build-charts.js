f13cc2a7f9220eae58ef28540d7ee73e63c7f6ad
phil-6 <github@purpleriver.dev>
2019-05-03 13:48:13 +0100

    get first example charts working

--- dashboard/assets/js/build-charts.js
+++ dashboard/assets/js/build-charts.js
@@ -59,3 +59,125 @@ var chartOne = new Chart(ctx, {
         }
     }
 });
+
+var ctx1 = document.getElementById('myDoughnutChart');
+
+var myDoughnutChart = new Chart(ctx1, {
+    type: 'doughnut',
+    data: {
+
+        datasets: [{
+            data: [
+                json.call_no_match_this_month,
+                json.call_match_home_this_month,
+                json.call_match_mobile_this_month,
+                json.call_match_alt_this_month,
+                json.call_collection_booked_this_month,
+                json.call_collection_not_booked_this_month
+            ],
+            backgroundColor: [
+                'red',
+                'orange',
+                'yellow',
+                'green',
+                'blue'
+            ],
+            label: [
+                'No Match',
+                'Match - Home',
+                'Match - Mob',
+                'Match - Alt',
+                'Collection Booked',
+                'Collection Not Booked'
+            ]
+        }]
+    },
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top'
+        },
+        title: {
+            display: true,
+            text: ' Doughnut Chart'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        }
+    }
+});
+
+var ctx2 = document.getElementById('myLineChart');
+
+var myLineChart = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: [
+            'No Match',
+            'Match - Home',
+            'Match - Mob',
+            'Match - Alt',
+            'Collection Booked',
+            'Collection Not Booked'
+        ],
+        datasets: [{
+            label: 'This Month',
+            backgroundColor: '#663399',
+            borderColor: '#663399',
+            data: [
+                json.call_no_match_this_month,
+                json.call_match_home_this_month,
+                json.call_match_mobile_this_month,
+                json.call_match_alt_this_month,
+                json.call_collection_booked_this_month,
+                json.call_collection_not_booked_this_month
+            ],
+            fill: false
+        }, {
+            label: 'Last Month',
+            fill: false,
+            backgroundColor: '#996633',
+            borderColor: '#996633',
+            data: [
+                json.call_no_match_last_month,
+                json.call_match_home_last_month,
+                json.call_match_mobile_last_month,
+                json.call_match_alt_last_month,
+                json.call_collection_booked_last_month,
+                json.call_collection_not_booked_last_month
+            ]
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Chart Title'
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Month'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Value'
+                }
+            }]
+        }
+    }
+});
